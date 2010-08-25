c38498ee9a4cac57dfbf68202a03b74228b92796
Daniel P. Berrange <berrange@redhat.com>
2010-08-25 18:04:38 +0100

    Add formal RPM build infrastructure
    * autogen.sh: Rewrite to use gnome-autogen.sh
    * autobuild.sh: Automated RPM build control script
    * configure.ac: Generate RPM specfiles
    * Makefile.am: Distribute RPM specfile templates
    * libosinfo.spec.in, mingw32-libosinfo.spec.in: RPM specs
    * osinfo/Makefile.am, osinfo/libosinfo.syms: Fix Win32
      portability problem

---
+++ autobuild.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+set -e
+set -v
+
+# Make things clean.
+test -f Makefile && make -k distclean || :
+
+rm -rf build
+mkdir build
+cd build
+
+../autogen.sh --prefix=$AUTOBUILD_INSTALL_ROOT \
+    --enable-compile-warnings=error
+
+make
+make install
+
+rm -f *.tar.gz
+make dist
+
+if [ -n "$AUTOBUILD_COUNTER" ]; then
+  EXTRA_RELEASE=".auto$AUTOBUILD_COUNTER"
+else
+  NOW=`date +"%s"`
+  EXTRA_RELEASE=".$USER$NOW"
+fi
+
+if [ -f /usr/bin/rpmbuild ]; then
+  rpmbuild --nodeps \
+     --define "extra_release $EXTRA_RELEASE" \
+     --define "_sourcedir `pwd`" \
+     -ba --clean libosinfo.spec
+fi
+
+if [ -x /usr/bin/i686-pc-mingw32-gcc ]; then
+  make distclean
+
+  PKG_CONFIG_PATH="$AUTOBUILD_INSTALL_ROOT/i686-pc-mingw32/sys-root/mingw/lib/pkgconfig:/usr/i686-pc-mingw32/sys-root/mingw/lib/pkgconfig" \
+  CC="i686-pc-mingw32-gcc" \
+  ../configure \
+    --build=$(uname -m)-pc-linux \
+    --host=i686-pc-mingw32 \
+    --enable-introspection=no \
+    --prefix="$AUTOBUILD_INSTALL_ROOT/i686-pc-mingw32/sys-root/mingw" \
+
+  make
+  make install
+
+  #set -o pipefail
+  #make check 2>&1 | tee "$RESULTS"
+
+  if [ -f /usr/bin/rpmbuild ]; then
+    rpmbuild --nodeps \
+       --define "extra_release $EXTRA_RELEASE" \
+       --define "_sourcedir `pwd`" \
+       -ba --clean mingw32-libosinfo.spec
+  fi
+fi
