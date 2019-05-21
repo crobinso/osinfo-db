54ebd04f5243badfeb272e63478b77ed84c4454d
Pino Toscano <ptoscano@redhat.com>
2019-05-21 17:21:09 +0200

    tests: test_dates: work without datetime.date.fromisoformat()
    datetime.date.fromisoformat() was introduced in Python 3.7, so provide
    an alternative implementation for it with older Python versions.
    
    Fixes commit 5da3b8fdd836a55a58365718e93d0372fcc2bf0b.
    
    Reviewed-by: Fabiano Fidêncio <fidencio@redhat.com>

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -2,14 +2,26 @@
 # See the COPYING file in the top-level directory.
 
 import datetime
+import re
+import sys
 
 from . import util
+
+
+if sys.version_info >= (3, 7):
+    def _parse_iso_date(date_string):
+        return datetime.date.fromisoformat(date_string)
+else:
+    def _parse_iso_date(date_string):
+        m = re.match("([0-9]{4})-([0-9]{2})-([0-9]{2})", date_string)
+        assert m
+        return datetime.date(int(m.group(1)), int(m.group(2)), int(m.group(3)))
 
 
 def _parse_date(date_string):
     if not date_string:
         return None
-    return datetime.date.fromisoformat(date_string)
+    return _parse_iso_date(date_string)
 
 
 @util.os_parametrize('osxml', filter_dates=True)
