b30b57abd33547aabf46cb761ecf63a43934f1a6
Pino Toscano <ptoscano@redhat.com>
2019-07-29 16:51:30 +0200

    tests: add a new test for variants
    Add a new test for checks related to variants.  The two subtests added
    ensure that the variant of a media or a tree is actually one of the
    variants declared by the OS.
    
    Reviewed-by: Fabiano Fidêncio <fidencio@redhat.com>

---
+++ tests/test_variants.py
@@ -0,0 +1,29 @@
+# This work is licensed under the GNU GPLv2 or later.
+# See the COPYING file in the top-level directory.
+
+import re
+import sys
+
+from . import util
+
+
+@util.os_parametrize('osxml', filter_media=True)
+def test_existing_variant_for_media(osxml):
+    variants = osxml.variants
+    for media in osxml.medias:
+        variant = media.variant
+        if variant and variant not in variants:
+            raise AssertionError("variants: media in OS %s has variant=%s "
+                                 "but declared variants are %s" %
+                                 (osxml.shortid, variant, variants))
+
+
+@util.os_parametrize('osxml', filter_trees=True)
+def test_existing_variant_for_trees(osxml):
+    variants = osxml.variants
+    for tree in osxml.trees:
+        variant = tree.variant
+        if variant and variant not in variants:
+            raise AssertionError("variants: tree in OS %s has variant=%s "
+                                 "but declared variants are %s" %
+                                 (osxml.shortid, variant, variants))
