152a6e48e082427913074860089201e4917c0d9d
Fabiano Fidêncio <fidencio@redhat.com>
2019-08-09 10:03:18 +0200

    tests,test_ids: Take into consideration extensions
    Extensions have their directories ending with ".d" and any name is a
    valid name for the files inside this directory.
    
    For instance, a fedora-30 extension would be:
    /path/to/db/os/fedoraproject.org/fedora-30.d/memory.xml
    
    This file is totally valid as long as memory.xml has its id as:
    http://fedoraproject.org/fedora/30
    
    This will be needed, later on, in order to validate more than OSes ids
    as we rely on extensions for, for instance, our devices.
    
    https://gitlab.com/libosinfo/osinfo-db/issues/33
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Cole Robinson <crobinso@redhat.com>

--- tests/test_ids.py
+++ tests/test_ids.py
@@ -13,15 +13,22 @@ def _test_validate_ids(xml, entity_type):
     This check tries to mimic, in a pythonic way, the very same
     check done by OsinfoLoader::osinfo_loader_check_id()
     """
+    relpath = os.path.relpath(xml.filename)
+    expected_filename = relpath.split(entity_type + "/")[1]
+
+    extension = bool(".d/" in expected_filename)
+
     suffix = xml.internal_id[len('http://'):]
     vendor = suffix.split('/', 1)[0]
     entity_name = suffix.split('/', 1)[1].replace("/", "-")
 
-    filename = vendor + "/" + entity_name + ".xml"
-    relpath = os.path.relpath(xml.filename)
-    expected_filename = relpath.split(entity_type + "/")[1]
+    if extension:
+        filename = vendor + "/" + entity_name + ".d"
+        assert filename == expected_filename.rsplit("/", 1)[0]
+    else:
+        filename = vendor + "/" + entity_name + ".xml"
+        assert filename == expected_filename
 
-    assert filename == expected_filename
 
 
 @util.os_parametrize('osxml')
