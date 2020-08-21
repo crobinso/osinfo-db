9a70ffd13d30cf501336fc388b76739842d2dc81
Pino Toscano <ptoscano@redhat.com>
2020-08-21 11:34:57 +0200

    tests: test_variants: check also images
    Since images can have variants as well, check that the specified
    variants are defined OS variants. This mimics the same checks done for
    media and trees.
    
    Signed-off-by: Pino Toscano <ptoscano@redhat.com>

--- tests/test_variants.py
+++ tests/test_variants.py
@@ -24,3 +24,14 @@ def test_existing_variant_for_trees(osxml):
             raise AssertionError("variants: tree in OS %s has variant=%s "
                                  "but declared variants are %s" %
                                  (osxml.shortid, variant, variants))
+
+
+@util.os_parametrize('osxml', filter_images=True)
+def test_existing_variant_for_images(osxml):
+    variants = osxml.variants
+    for image in osxml.images:
+        variant = image.variant
+        if variant and variant not in variants:
+            raise AssertionError("variants: image in OS %s has variant=%s "
+                                 "but declared variants are %s" %
+                                 (osxml.shortid, variant, variants))
