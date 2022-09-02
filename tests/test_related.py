f56874eed9f4c615cfef821f5edf43625d54d510
Pino Toscano <ptoscano@redhat.com>
2022-09-02 08:47:01 +0200

    tests: check also for <upgrades> pointing to self
    test_related used to check for self-pointing references in <clones> and
    <derives-from>; check also <upgrades>.
    
    Signed-off-by: Pino Toscano <ptoscano@redhat.com>

--- tests/test_related.py
+++ tests/test_related.py
@@ -7,6 +7,11 @@ from . import util
 @util.os_parametrize('osxml', filter_related=True)
 def test_related(osxml):
     related = util.DataFiles.getosxml_related(osxml)
+    upgrades = osxml.upgrades
+    if upgrades:
+        found = util.DataFiles.getosxml_by_id(upgrades)
+        assert found
+        related.append(found)
 
     for os in related:
         assert os is not osxml
