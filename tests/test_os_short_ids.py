2a5ff068bade38031e1195f338270df4ab7c3b7e
Pino Toscano <ptoscano@redhat.com>
2023-03-09 13:00:29 +0100

    tests: check for duplicate short IDs
    Add a simple test that checks whether there are short IDs used in more
    than one OS; it may happen e.g. because of typos.
    
    There are no problems reported currently.
    
    Signed-off-by: Pino Toscano <ptoscano@redhat.com>

--- tests/test_os_short_ids.py
+++ tests/test_os_short_ids.py
@@ -1,6 +1,8 @@
 # This work is licensed under the GNU GPLv2 or later.
 # See the COPYING file in the top-level directory.
 
+import collections
+import logging
 import re
 
 from . import util
@@ -17,3 +19,19 @@ def test_validate_short_ids(osxml):
             invalids.append(shortid)
 
     assert invalids == []
+
+
+def test_duplicate_short_ids():
+    ids = collections.defaultdict(set)
+    # collect the mapping of short-id -> IDs that have it
+    for osxml in util.DataFiles.oses():
+        for shortid in osxml.shortids:
+            ids[shortid].add(osxml.internal_id)
+    # drop all the mappings with only one ID
+    for shortid in list(ids.keys()):
+        if len(ids[shortid]) == 1:
+            del ids[shortid]
+    # log all the problematic ones
+    for shortid, ids in ids.items():
+        logging.info("shortid=%s is used by %s", shortid, sorted(list(ids)))
+    assert ids == {}
