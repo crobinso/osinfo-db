a6f2b17ea990c0f5fab8a1761e7c1c4c31c571e0
Pino Toscano <ptoscano@redhat.com>
2023-05-19 19:15:07 +0200

    tests: check for no translations in source files
    Make sure that there are no xml:lang attributes in source files.
    
    Signed-off-by: Pino Toscano <ptoscano@redhat.com>

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -16,3 +16,9 @@ def test_schema(path):
     if not RELAXNG.validate(ET.parse(path.open("r"))):
         # pylint: disable=no-member
         raise AssertionError(str(RELAXNG.error_log.last_error))
+
+
+@pytest.mark.parametrize("path", util.SourceFiles.xmls(), ids=lambda p: p.name)
+def test_translations_in_source_files(path):
+    xml = ET.parse(path.open("r"))
+    assert xml.xpath("//*[@xml:lang]") == []
