0a488a27dd6408f1c0bfe7f68fc97342a99d2556
Pino Toscano <ptoscano@redhat.com>
2023-06-25 17:55:35 +0200

    tests: check ISO languages using .lng files
    Among the isodata test files, there are also ".lng" files associated
    with the ".txt" ISO files: they are used to specify the languages of an
    ISO file; this feature is mostly used for Windows ISOs, and it was
    introduced in the libosinfo test suite long ago [1] (then copied over to
    osinfo-db).
    
    Make again use of these data in test_isoinfo:
    - for each isodata file, read the languages available for it, if present
    - in case there are languages and the ISO matches the wanted OS, try to
      read the languages from the <iso> definition for the actual ISO using
      the same logic used in libosinfo, and then compare it with what is
      expected
    
    All the existing ".lng" files work fine, no regression was found.
    
    Fixes: #38
    
    [1] https://gitlab.com/libosinfo/libosinfo/-/commit/ac8c5ef7f57505dbd68d9094c0c3e6de247478b7
    
    Signed-off-by: Pino Toscano <ptoscano@redhat.com>

--- tests/test_isoinfo.py
+++ tests/test_isoinfo.py
@@ -1,12 +1,27 @@
 # This work is licensed under the GNU GPLv2 or later.
 # See the COPYING file in the top-level directory.
 
+import configparser
+import contextlib
 import logging
 from pathlib import Path
+import re
 import pytest
 
 from . import util
 from . import isodata
+
+
+def _load_languages(isofile):
+    assert isofile.suffix == ".txt"
+    langfile = isofile.with_suffix(".lng")
+    config = configparser.ConfigParser()
+    try:
+        with langfile.open("r") as f:
+            config.read_file(f)
+    except FileNotFoundError:
+        return None
+    return set(config.get("general", "l10n-language").split(","))
 
 
 def _get_isodatapaths():
@@ -21,16 +36,47 @@ def _get_isodatapaths():
     osdirs = [d for d in isodata_path.glob(str(Path("*", "*"))) if d.is_dir()]
 
     for osdir in sorted(osdirs, key=util.path_sort):
-        isodatapaths = list(osdir.glob("*.txt"))
-        if len(isodatapaths):
+        isofilepaths = list(osdir.glob("*.txt"))
+        if len(isofilepaths):
+            isodatapaths = []
+            for isofile in isofilepaths:
+                isodatapaths.append((isofile, _load_languages(isofile)))
             ret.append(pytest.param(osdir.name, isodatapaths, id=osdir.name))
 
     return ret
 
 
+def _get_iso_languages(iso, isodatamedia):
+    languages = set()
+    regex = None
+    l10n_language_map = None
+
+    for l10n_language in iso.l10n_languages:
+        value = l10n_language.value
+        if l10n_language.regex:
+            regex = value
+            if l10n_language.l10n_language_map:
+                l10n_language_map = l10n_language.l10n_language_map
+        else:
+            languages.add(value)
+
+    # This matches the same logic in libosinfo (set_languages_for_media()
+    # in OsinfoDb): if there is a regex specification for the language,
+    # the result is used overring any other language previously specified
+    if regex and isodatamedia.volumeid:
+        m = re.match(regex, isodatamedia.volumeid)
+        if m and m.lastindex:
+            lang = m.group(1)
+            with contextlib.suppress(KeyError):
+                lang = util.DataFiles.datamaps()[l10n_language_map][lang]
+            return {lang}
+
+    return languages
+
+
 @pytest.mark.parametrize("osname,isodatapaths", _get_isodatapaths())
 def test_iso_detection(osname, isodatapaths):
-    for isodatapath in isodatapaths:
+    for isodatapath, languages in isodatapaths:
         # A symlink indicates an ISO from another version
         # that is ambiguous and so accidentally matches
         # this distro too. We ignore them here, because
@@ -57,6 +103,11 @@ def test_iso_detection(osname, isodatapaths):
                             isodatamedia.path,
                             osxml2.shortid,
                         )
+                        if languages:
+                            # If there are languages specified for this ISO,
+                            # check they match what described in <iso>
+                            iso_languages = _get_iso_languages(media.iso, isodatamedia)
+                            assert languages == iso_languages
                     # For several distros we do not have the volume-size
                     # set as part of our DB, thus multiple detections may
                     # occur. Although this case is not the optimal, as long
